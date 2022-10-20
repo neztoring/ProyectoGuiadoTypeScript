d9ca2de20d81ba80f765877da91c8b625435a1ef
Nestor Pérez Espinel <neztoring@gmail.com>
2022-10-20 06:14:34 +0000

    Filtar Cursos

--- main.ts
+++ main.ts
@@ -13,10 +13,36 @@ console.log(ap.cursos);
 let aprendizTable:HTMLElement = document.getElementById("aprendiz")!;
 let estadisticasTable:HTMLElement = document.getElementById("estadisticas")!;
 let cursosTable:HTMLElement = document.getElementById("cursos")!;
+let btnFiltro:HTMLElement = document.getElementById("boton-filtro")!;
+let textoBusqueda: HTMLInputElement = <HTMLInputElement>document.getElementById("texto-busqueda")!;
+
+//btnFiltro.onclick= filtarPorNombre;
+
+//funcion anonima
+btnFiltro.onclick =() =>{
+
+        let text:string= textoBusqueda.value;
+        text=(text==null)?"":text;
+        cursosTable.getElementsByTagName("tbody")[0].remove();
+        let cursosFiltrados: Curso[]=ap.cursos.filter(function(c){ return c.nombre.match(text);})
+        mostrarCursosAprendiz(cursosFiltrados);
+    
+};
 
 mostrarDatosAprendiz(ap);
 mostrarEstadisticas(ap);
-mostrarCursosAprendiz(ap);
+mostrarCursosAprendiz(ap.cursos);
+
+
+function filtarPorNombre():void{
+    let text:string= textoBusqueda.value;
+    text=(text==null)?"":text;
+    cursosTable.getElementsByTagName("tbody")[0].remove();
+    //let cursosFiltrados: Curso[]=ap.cursos.filter(function(c){ return c.nombre.match(text);})
+    let cursosFiltrados: Curso[]=ap.cursos.filter(c=> c.nombre.match(text))
+    mostrarCursosAprendiz(cursosFiltrados);
+}
+
 
 function mostrarDatosAprendiz(aprendiz: Aprendiz):void{
     let tbodyAprendiz=document.createElement("tbody");
@@ -40,9 +66,9 @@ function mostrarEstadisticas(aprendiz: Aprendiz):void{
 
 
 
-function mostrarCursosAprendiz(aprendiz: Aprendiz):void{
+function mostrarCursosAprendiz(cursos: Curso[]):void{
     let cursosTbody=document.createElement("tbody");
-    for(let curso of aprendiz.cursos)
+    for(let curso of cursos)
     {
         let trElement:HTMLElement= document.createElement("tr");
         trElement.innerHTML = `<td>${curso.nombre}</td>
--- scripts/main.js
+++ scripts/main.js
@@ -10,9 +10,28 @@ console.log(ap.cursos);
 var aprendizTable = document.getElementById("aprendiz");
 var estadisticasTable = document.getElementById("estadisticas");
 var cursosTable = document.getElementById("cursos");
+var btnFiltro = document.getElementById("boton-filtro");
+var textoBusqueda = document.getElementById("texto-busqueda");
+//btnFiltro.onclick= filtarPorNombre;
+//funcion anonima
+btnFiltro.onclick = function () {
+    var text = textoBusqueda.value;
+    text = (text == null) ? "" : text;
+    cursosTable.getElementsByTagName("tbody")[0].remove();
+    var cursosFiltrados = ap.cursos.filter(function (c) { return c.nombre.match(text); });
+    mostrarCursosAprendiz(cursosFiltrados);
+};
 mostrarDatosAprendiz(ap);
 mostrarEstadisticas(ap);
-mostrarCursosAprendiz(ap);
+mostrarCursosAprendiz(ap.cursos);
+function filtarPorNombre() {
+    var text = textoBusqueda.value;
+    text = (text == null) ? "" : text;
+    cursosTable.getElementsByTagName("tbody")[0].remove();
+    //let cursosFiltrados: Curso[]=ap.cursos.filter(function(c){ return c.nombre.match(text);})
+    var cursosFiltrados = ap.cursos.filter(function (c) { return c.nombre.match(text); });
+    mostrarCursosAprendiz(cursosFiltrados);
+}
 function mostrarDatosAprendiz(aprendiz) {
     var tbodyAprendiz = document.createElement("tbody");
     tbodyAprendiz.innerHTML = "<tr><td colspan=2><img src=\"./" + aprendiz.avatar + "\" height=\"100\"/></td><tr>\n    <tr><td>Nombres:</td><td>" + aprendiz.nombres + "</td><tr>\n    <tr><td>Apellidos:</td><td>" + aprendiz.apellidos + "</td><tr>\n    <tr><td>Nivel Educativo:</td><td>" + aprendiz.nivelEducativo + "</td><tr>\n    <tr><td>Edad:</td><td>" + aprendiz.edad + "</td><tr>";
@@ -24,10 +43,10 @@ function mostrarEstadisticas(aprendiz) {
     trElement.innerHTML = "<td><b>Cursos Certificados</b></td><td>" + numeroCertificados + "</td>";
     estadisticasTable.appendChild(trElement);
 }
-function mostrarCursosAprendiz(aprendiz) {
+function mostrarCursosAprendiz(cursos) {
     var cursosTbody = document.createElement("tbody");
-    for (var _i = 0, _a = aprendiz.cursos; _i < _a.length; _i++) {
-        var curso = _a[_i];
+    for (var _i = 0, cursos_1 = cursos; _i < cursos_1.length; _i++) {
+        var curso = cursos_1[_i];
         var trElement = document.createElement("tr");
         trElement.innerHTML = "<td>" + curso.nombre + "</td>\n        <td>" + curso.horas + "</td>\n        <td>" + curso.calificacion + "</td>\n        <td>" + curso.certificado + "</td>\n        <td>" + curso.anio + "</td>";
         cursosTbody.appendChild(trElement);
