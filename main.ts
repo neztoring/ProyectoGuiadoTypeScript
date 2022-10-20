1495143358e3e2b9928d80d7bac4345d143302c1
Nestor Pérez Espinel <neztoring@gmail.com>
2022-10-20 05:52:18 +0000

    Listar Cursos

--- main.ts
+++ main.ts
@@ -12,9 +12,11 @@ console.log(ap.cursos);
 
 let aprendizTable:HTMLElement = document.getElementById("aprendiz")!;
 let estadisticasTable:HTMLElement = document.getElementById("estadisticas")!;
+let cursosTable:HTMLElement = document.getElementById("cursos")!;
 
 mostrarDatosAprendiz(ap);
 mostrarEstadisticas(ap);
+mostrarCursosAprendiz(ap);
 
 function mostrarDatosAprendiz(aprendiz: Aprendiz):void{
     let tbodyAprendiz=document.createElement("tbody");
@@ -33,6 +35,24 @@ function mostrarEstadisticas(aprendiz: Aprendiz):void{
     let trElement:HTMLElement= document.createElement("tr");
     trElement.innerHTML = `<td><b>Cursos Certificados</b></td><td>${numeroCertificados}</td>`;
     estadisticasTable.appendChild(trElement);
-
     
+}
+
+
+
+function mostrarCursosAprendiz(aprendiz: Aprendiz):void{
+    let cursosTbody=document.createElement("tbody");
+    for(let curso of aprendiz.cursos)
+    {
+        let trElement:HTMLElement= document.createElement("tr");
+        trElement.innerHTML = `<td>${curso.nombre}</td>
+        <td>${curso.horas}</td>
+        <td>${curso.calificacion}</td>
+        <td>${curso.certificado}</td>
+        <td>${curso.anio}</td>`
+        cursosTbody.appendChild(trElement);
+    }
+
+    cursosTable.appendChild(cursosTbody);
+
 }
--- scripts/main.js
+++ scripts/main.js
@@ -9,8 +9,10 @@ export var ap = new Aprendiz("Nestor", "Pérez Espinel", "avatar.png", 43, Nivel
 console.log(ap.cursos);
 var aprendizTable = document.getElementById("aprendiz");
 var estadisticasTable = document.getElementById("estadisticas");
+var cursosTable = document.getElementById("cursos");
 mostrarDatosAprendiz(ap);
 mostrarEstadisticas(ap);
+mostrarCursosAprendiz(ap);
 function mostrarDatosAprendiz(aprendiz) {
     var tbodyAprendiz = document.createElement("tbody");
     tbodyAprendiz.innerHTML = "<tr><td colspan=2><img src=\"./" + aprendiz.avatar + "\" height=\"100\"/></td><tr>\n    <tr><td>Nombres:</td><td>" + aprendiz.nombres + "</td><tr>\n    <tr><td>Apellidos:</td><td>" + aprendiz.apellidos + "</td><tr>\n    <tr><td>Nivel Educativo:</td><td>" + aprendiz.nivelEducativo + "</td><tr>\n    <tr><td>Edad:</td><td>" + aprendiz.edad + "</td><tr>";
@@ -21,4 +23,14 @@ function mostrarEstadisticas(aprendiz) {
     var trElement = document.createElement("tr");
     trElement.innerHTML = "<td><b>Cursos Certificados</b></td><td>" + numeroCertificados + "</td>";
     estadisticasTable.appendChild(trElement);
+}
+function mostrarCursosAprendiz(aprendiz) {
+    var cursosTbody = document.createElement("tbody");
+    for (var _i = 0, _a = aprendiz.cursos; _i < _a.length; _i++) {
+        var curso = _a[_i];
+        var trElement = document.createElement("tr");
+        trElement.innerHTML = "<td>" + curso.nombre + "</td>\n        <td>" + curso.horas + "</td>\n        <td>" + curso.calificacion + "</td>\n        <td>" + curso.certificado + "</td>\n        <td>" + curso.anio + "</td>";
+        cursosTbody.appendChild(trElement);
+    }
+    cursosTable.appendChild(cursosTbody);
 }
