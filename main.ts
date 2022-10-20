ab15310dd048e12e44d7d4d3fc27761da932e2ee
Nestor Pérez Espinel <neztoring@gmail.com>
2022-10-20 03:55:39 +0000

    Implementar Aprendiz

---
+++ main.ts
@@ -0,0 +1,5 @@
+import { Aprendiz, NivelEducativo } from "./aprendiz.js";
+
+export const ap = new Aprendiz("Nestor","Pérez Espinel","avatar.png",43,NivelEducativo.POSGRADO);
+
+console.log(ap);
---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+import { Aprendiz, NivelEducativo } from "./aprendiz.js";
+export var ap = new Aprendiz("Nestor", "Pérez Espinel", "avatar.png", 43, NivelEducativo.POSGRADO);
+console.log(ap);
